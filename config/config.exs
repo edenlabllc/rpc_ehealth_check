7626892e1c47bb7d1fbd4e4a87201768f6461b01
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-06-12 13:21:08 +0300

    feat: check only required nodes (#5)

--- config/config.exs
+++ config/config.exs
@@ -18,13 +18,84 @@ config :logger,
 
 config :health_check,
   topologies: [
-    k8s_me_event_consumer: [
+    k8s_ael_api: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "ael_api",
+        kubernetes_selector: "app=api",
+        kubernetes_namespace: "ael",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_ds_api: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "ds_api",
+        kubernetes_selector: "app=ds-api",
+        kubernetes_namespace: "digital-signature",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_ds_synchronizer: [
       strategy: Elixir.Cluster.Strategy.Kubernetes,
       config: [
         mode: :dns,
         kubernetes_ip_lookup_mode: :pods,
-        kubernetes_node_basename: "event_consumer",
-        kubernetes_selector: "app=event-consumer",
+        kubernetes_node_basename: "synchronizer_crl",
+        kubernetes_selector: "app=synchronizer-crl",
+        kubernetes_namespace: "digital-signature",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_edr_api: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_ip_lookup_mode: :pods,
+        kubernetes_node_basename: "edr_api",
+        kubernetes_selector: "app=edr-api",
+        kubernetes_namespace: "edr",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_ehealth_api: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "ehealth",
+        kubernetes_selector: "app=api",
+        kubernetes_namespace: "il",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_casher: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "casher",
+        kubernetes_selector: "app=casher",
+        kubernetes_namespace: "il",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_jabba: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "jabba-rpc",
+        kubernetes_selector: "app=jabba-rpc",
+        kubernetes_namespace: "jabba",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_me: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "medical_events_api",
+        kubernetes_selector: "app=api-medical-events",
         kubernetes_namespace: "me",
         polling_interval: 10_000
       ]
@@ -40,24 +111,83 @@ config :health_check,
         polling_interval: 10_000
       ]
     ],
-    k8s_edr_api: [
+    k8s_number_generator: [
       strategy: Elixir.Cluster.Strategy.Kubernetes,
       config: [
         mode: :dns,
-        kubernetes_ip_lookup_mode: :pods,
-        kubernetes_node_basename: "edr_api",
-        kubernetes_selector: "app=edr-api",
-        kubernetes_namespace: "edr",
+        kubernetes_node_basename: "number_generator",
+        kubernetes_selector: "app=number-generator",
+        kubernetes_namespace: "me",
         polling_interval: 10_000
       ]
     ],
-    k8s_jabba: [
+    k8s_mithril: [
       strategy: Elixir.Cluster.Strategy.Kubernetes,
       config: [
         mode: :dns,
-        kubernetes_node_basename: "jabba-rpc",
-        kubernetes_selector: "app=jabba-rpc",
-        kubernetes_namespace: "jabba",
+        kubernetes_node_basename: "mithril_api",
+        kubernetes_selector: "app=api",
+        kubernetes_namespace: "mithril",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_mpi_api: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "mpi",
+        kubernetes_selector: "app=api",
+        kubernetes_namespace: "mpi",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_manual_merger: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "manual_merger",
+        kubernetes_selector: "app=manual-merger",
+        kubernetes_namespace: "mpi",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_ops: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "ops",
+        kubernetes_selector: "app=api",
+        kubernetes_namespace: "ops",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_report_cache: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "report_cache",
+        kubernetes_selector: "app=cache",
+        kubernetes_namespace: "reports",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_uaddresses: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "uaddresses_api",
+        kubernetes_selector: "app=api",
+        kubernetes_namespace: "uaddresses",
+        polling_interval: 10_000
+      ]
+    ],
+    k8s_otp_verification: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      config: [
+        mode: :dns,
+        kubernetes_node_basename: "otp_verification_api",
+        kubernetes_selector: "app=api",
+        kubernetes_namespace: "verification",
         polling_interval: 10_000
       ]
     ]
