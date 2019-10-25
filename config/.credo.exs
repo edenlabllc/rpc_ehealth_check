7abd656beb3a6d12ac4eee35d92467e36c442007
Alex Kovalevych <alexkovalevych@gmail.com>
2019-10-25 16:48:15 +0300

    feat: elixir 1.9

--- config/.credo.exs
+++ config/.credo.exs
@@ -8,6 +8,8 @@
         excluded: ["apps/*/lib/tasks/tasks.ex"]
       },
       checks: [
+        {Credo.Check.Refactor.MapInto, false},
+        {Credo.Check.Warning.LazyLogging, false},
         {Credo.Check.Design.DuplicatedCode, false},
         {Credo.Check.Design.TagTODO, exit_status: 0},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 120},
